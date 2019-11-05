6a8aecae1521f7c9fc9276e07ac22ec74f7eaae5
David <david.baptista00@hotmail.com>
2019-11-05 16:57:06 +0000

    shell script quase feito. Falta verificacao de erros

--- runTests.sh
+++ runTests.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-if [ $# -lt 2 ] ; then
-    echo "Example: ./runTests.sh inputdir outputdir maxthreads numbuckets"
-    exit 1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+if [ $# -ne 4 ] ; then
+	echo -e "${RED}Invalid number of arguments ${NC}"
+	echo -e "${BLUE}Example: ./runTests.sh inputdir outputdir maxthreads numbuckets ${NC}"
+	exit 1
+fi
+
+INPUTDIR="$1"
+OUTPUTDIR="$2"
+MAXTHREADS="$3"
+NUMBUCKETS="$4"
+
+NOSYNC="tecnicofs-nosync"
+MUTEX="tecnicofs-mutex"
+
+GOOD=1
+for file_in in `ls -rS ${INPUTDIR}/*.txt`; do
+    file_out="${OUTPUTDIR}/${file_in#*/}"
+	echo "OUTPUT FILE ${file_out}"
+	if [${MAXTHREADS} == 1]; then
+		echo -e "InputFile=${file_in} NumThreads=1"
+		(./${NOSYNC} ${file_in} ${file_out} 1 ${NUMBUCKETS})
+	else
+		for ((i=2;i<=MAXTHREADS;i++)); do
+			echo -e "InputFile=${file_in} NumThreads=${i}"
+			(./${MUTEX} ${file_in} ${file_out} ${i} ${NUMBUCKETS})
+		done
+	fi
+done
+
+if [ ${GOOD} == 1 ]; then
+    echo -e "${GREEN}+++++++++++++++++"
+    echo "Passed all tests"
+    echo -e "+++++++++++++++++${NC}"
 fi
