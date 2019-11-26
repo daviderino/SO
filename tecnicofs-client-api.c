749a96c3f49639e6150a6a78af66d850eb40a6c1
catarina <catarina.alegria@gmail.com>
2019-11-26 14:54:57 +0000

    oi

--- tecnicofs-client-api.c
+++ tecnicofs-client-api.c
@@ -63,3 +63,70 @@ int tfsCreate(char *filename, int ownerPermissions, int othersPermissions) {
 
     return 0;
 }
+
+int tfsOpen (char *filename, int mode){
+    char buffer[BUFFSIZE];
+
+    sprintf(buffer, "o %s %d", filename, mode);
+
+     if(write(sockfd, buffer, BUFFSIZE) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+     if(read(sockfd, buffer, BUFFSIZE) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+    if(!strcmp(buffer, "PERMISSION_DENIED")) {
+        return TECNICOFS_ERROR_PERMISSION_DENIED;
+    }
+
+    if(!strcmp(buffer, "FILE_DOESNT_EXISTS")) {
+        return TECNICOFS_ERROR_FILE_NOT_FOUND;
+    }
+
+    if(!strcmp(buffer, "MAX_FILES_OPEN")) {
+        return TECNICOFS_ERROR_MAXED_OPEN_FILES;
+    }
+
+    return (int)strtol(buffer,NULL,10);
+
+}
+
+int tfsRead (int fd, char *buffer, int len){
+    char buff[BUFFSIZE];
+
+    sprintf(buff, "l %d %d", fd, len);
+
+     if(write(sockfd, buff,BUFFSIZE) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+    if(read(sockfd, buffer, len) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+    return strlen(buffer);
+
+}
+
+int tfsClose(int fd){
+    char buffer[BUFFSIZE];
+
+    sprintf(buffer, "c %d", fd);
+
+    if(write(sockfd, buffer, BUFFSIZE) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+    if(read(sockfd, buffer, BUFFSIZE) < 0) {
+        return TECNICOFS_ERROR_OTHER;
+    }
+
+    if(!strcmp(buffer, "FILE_NOT_OPEN")) {
+        return TECNICOFS_ERROR_FILE_NOT_OPEN;
+    }
+
+    return 0;
+
+}
